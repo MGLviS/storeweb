b37f47957ebddeb9ae3877699ae2120d63686b0f
LuisM <118645322+MGLviS@users.noreply.github.com>
2023-11-04 02:14:00 -0500

    Initial Commit

---
+++ src/components/login-form.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+        <q-input v-model="username" filled type="username" hint="username" />
+    <br />
+    <br />
+        <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="Password with toggle">
+            <template v-slot:append>
+            <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+            />
+            </template>
+      </q-input>
+    </div>
+    </div>
+
+</template>
+
+
+<style>
+
+</style>
+
+
+<script>
+
+</script>
